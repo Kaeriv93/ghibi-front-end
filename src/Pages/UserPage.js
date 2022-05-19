ffb6d05de520e5746f56850a68072dddbb3cb5cd
kaeriv93 <vsan93@yahoo.com>
2022-05-19 14:59:16 -0500

    can edit more stuff

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -7,8 +7,13 @@ const UserPage = (props) =>{
     let {id} = useParams()
     let users = props.user
     let user = users.find(u => u._id === id)
+    let favoriteFilm = users.favorites
 
     const[editForm, setEditForm] = useState(user)
+    const [fav, setFav] = useState({
+        favorites:[{favoriteFilm}],
+        newfavorites:[{}]
+    })
 
     const handleChange = event =>{
         setEditForm({...editForm, [event.target.name]:event.target.value})
@@ -25,11 +30,19 @@ const UserPage = (props) =>{
         navigate('/')
     }
 
+    const addForm =()=>{
+        props.addForm(editForm,id)
+        navigate(`/userpage/${id}`)
+    }
+
     return(
         <>
         <div className="profile">
             <div className="userpic">
                 <img src={user.profilepic} alt={user.firstName}/>
+                <form onSubmit={handleSubmit}>
+                    <input type ='text' name="profilepic" placeholder="Add an image to yourself!" onChange={handleChange} value={editForm.profilepic}/>
+                </form>
             </div>
 
             <div className="user-title">
@@ -53,6 +66,9 @@ const UserPage = (props) =>{
             <div className="favorites">
                 <h3>Favorites List</h3>
                 <h5>{user.favorites}</h5>
+                <form onSubmit={handleSubmit}>
+                    <input type ='text' name="favorites" placeholder="Add a favorite" onChange={handleChange} value={editForm.favorites}/>
+                </form>
             </div>
 
             <div className="reviews">
@@ -64,7 +80,7 @@ const UserPage = (props) =>{
                 <h4>Tom</h4>
                 <img src="https://pbs.twimg.com/profile_images/1237550450/mstom.jpg" alt="tom"/>
             </div>
-            <form onSubmit={handleSubmit} className="editform">
+            <form onSubmit={addForm} className="editform">
                 <input onChange={handleChange} type="text" name="bio" placeholder="Edit About Me" value={editForm.bio}/>
             </form>
             <div className="delete">
